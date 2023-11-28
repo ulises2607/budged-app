34cb39e7fa128055c6781635f5916132005979eb
Ulises <ulisescappa@gmail.com>
2023-11-28 16:02:17 -0300

    Add rspec, factoryBot and Faker to the Gemfile. Add users and payments factories. Add users and payments tests.

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,6 +1,8 @@
 class Payment < ApplicationRecord
-  belongs_to :user, foreign_key: :author
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
   validates :amount, presence: true, numericality: { greater_than: 0 }
   validates :author, presence: true
+  validates :name, presence: true
+
 end
---
+++ spec/factories/payments.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+    factory :payment do
+      name { Faker::Name.name }
+      amount { Faker::Number.decimal(l_digits: 3, r_digits: 2) }
+      association :author, factory: :user
+    end
+  end
+  
