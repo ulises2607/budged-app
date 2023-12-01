4f62b8614e68315a0cf88226ba62e0e732ec4d6f
Ulises <ulisescappa@gmail.com>
2023-12-01 16:38:39 -0300

    Show payments list ordered by the most recent

--- app/controllers/group_payments_controller.rb
+++ app/controllers/group_payments_controller.rb
@@ -2,7 +2,7 @@ class GroupPaymentsController < ApplicationController
 
     def index
         @group = Group.find(params[:group_id])
-        @payments = GroupPayment.where(group_id: @group.id).map(&:payment)
+        @payments = GroupPayment.where(group_id: @group.id).map(&:payment).sort_by(&:created_at).reverse
     end
 
 end
