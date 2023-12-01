84bd70b117506fd5680421629fe42ece436fe09a
Ulises <ulisescappa@gmail.com>
2023-12-01 16:40:44 -0300

    Fix linters errors

--- app/controllers/group_payments_controller.rb
+++ app/controllers/group_payments_controller.rb
@@ -1,8 +1,6 @@
 class GroupPaymentsController < ApplicationController
-
-    def index
-        @group = Group.find(params[:group_id])
-        @payments = GroupPayment.where(group_id: @group.id).map(&:payment).sort_by(&:created_at).reverse
-    end
-
+  def index
+    @group = Group.find(params[:group_id])
+    @payments = GroupPayment.where(group_id: @group.id).map(&:payment).sort_by(&:created_at).reverse
+  end
 end
--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,33 +1,31 @@
 class PaymentsController < ApplicationController
-    before_action :authenticate_user!
-    
-    def index
-        @payments = current_user.payments
-    end
-    
-    def new
-        @payment = Payment.new
-    end
-    
-    def create
+  before_action :authenticate_user!
 
-        @payment = Payment.new(payment_params)
-        @payment.author = current_user
-        @group = Group.find(params[:group_id])
-        puts @payment.inspect
-        if @payment.save
-        @group_payment = GroupPayment.new(group_id: params[:group_id], payment_id: @payment.id)
-        @group_payment.save
-        redirect_to group_group_payments_path(@group)
-        else
-        render :new
-        end
-    end
-    
-    private
-    
-    def payment_params
-        params.require(:payment).permit(:name, :amount)
-    end
+  def index
+    @payments = current_user.payments
+  end
 
+  def new
+    @payment = Payment.new
+  end
+
+  def create
+    @payment = Payment.new(payment_params)
+    @payment.author = current_user
+    @group = Group.find(params[:group_id])
+    puts @payment.inspect
+    if @payment.save
+      @group_payment = GroupPayment.new(group_id: params[:group_id], payment_id: @payment.id)
+      @group_payment.save
+      redirect_to group_group_payments_path(@group)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def payment_params
+    params.require(:payment).permit(:name, :amount)
+  end
 end
