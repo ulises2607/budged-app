40df654f8d8e564c7a29c2a1b03e307d2a292a58
Ulises <ulisescappa@gmail.com>
2023-12-02 05:41:32 -0300

    Fix rails_helper error. Move features folder to the right place.

--- spec/features/group_payments_spec.rb
+++ spec/features/group_payments_spec.rb
@@ -1,9 +1,8 @@
-# spec/features/group_payments_spec.rb
 require 'rails_helper'
 
 RSpec.feature 'Group Payments page', type: :feature do
   let(:user) { create(:user) }
-  let(:group) { create(:group, user:) }
+  let(:group) { create(:group, user: user) }
 
   before do
     visit new_user_session_path
@@ -15,14 +14,9 @@ RSpec.feature 'Group Payments page', type: :feature do
   scenario 'user views group payments page' do
     visit group_group_payments_path(group)
 
-    # Asegúrate de que la página se carga correctamente
     expect(page).to have_http_status(:success)
-
-    # Asegúrate de que la página tenga el contenido que esperas
     expect(page).to have_content('PAYMENTS')
   end
-
-  # Puedes agregar más escenarios de prueba según tus necesidades
 
   scenario 'user adds a new payment' do
     visit group_group_payments_path(group)
