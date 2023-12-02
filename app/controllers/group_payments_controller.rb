203cecdcbb4f21157ed9e944563ac89d7bdba205
Ulises <ulisescappa@gmail.com>
2023-12-02 17:48:01 -0300

    Fix n+1 problems in groups and group payment scontroller. Add association betwen Groupos and payments.

--- app/controllers/group_payments_controller.rb
+++ app/controllers/group_payments_controller.rb
@@ -1,7 +1,7 @@
 class GroupPaymentsController < ApplicationController
   def index
     @group = Group.find(params[:group_id])
-    @payments = GroupPayment.where(group_id: @group.id).map(&:payment).sort_by(&:created_at).reverse
+    @payments = @group.group_payments.includes(:payment).map(&:payment).sort_by(&:created_at).reverse
     @total = @payments.sum(&:amount)
   end
 end
